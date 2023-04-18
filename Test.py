dab96f3d0ae9ab40330a5a2fdb879c5275718bd5
pythonfor2023 <128965292+pythonfor2023@users.noreply.github.com>
2023-04-18 12:36:26 +0530

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,130 @@
+
+import random 
+import string
+
+
+#id=random()
+print(id)
+n=3
+res= ''.join(random.choices(string.ascii_uppercase+random.number,k=4))
+print("EMPN"+str(res))
+
+# print(dir(random))
+# ________:_________________________
+# list 
+
+simple=["hello","simple","koch"]
+print("lenght of the symbol",len(simple))
+print(simple)
+for i in range(0,len(simple)):
+    print("index:",i,"value:",simple[i],"\n")
+
+#     PS C:\Users\subrah> python3 Problem_of_day.py
+# helool inside simple34
+# hello sir
+# hello world
+# the size of the list: 104
+# pinting the memory allowactions fo the list: 2297500283264
+# ['subbu', 'hegde', 33, 667, 89, 90, 'Hello hii']
+# the size of the List after appending: 136
+# print the  or the location : 2297500283264
+# the updated list: ['subbu', 'hegde', 33, 667, 89, 90, 'Hello hii', 'sdsdsdsdsdsdsd']
+# the size: 136
+# the  find memeory locations if list: 2297500283264
+# ['subbu', 'hegde', 33, 667, 89, 90, 'Hello hii', 'sdsdsdsdsdsdsd', [2323232323, 23232323232322323]]
+# size: 136
+# id: 2297500283264
+# _________________________________tuple:________
+# PS C:\Users\subrah> python3 Problem_of_day.py
+#   File "Problem_of_day.py", line 70
+#     print("the size of the tuple:"sys.getsizeof(tuple))
+#                                   ^
+# SyntaxError: invalid syntax
+# PS C:\Users\subrah>
+# PS C:\Users\subrah> python3 Problem_of_day.py
+#   File "Problem_of_day.py", line 70
+#     print("the size of the tuple:"sys.getsizeof(tuple))
+#                                   ^
+# SyntaxError: invalid syntax
+# PS C:\Users\subrah> python3
+# Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:48:03) [MSC v.1928 64 bit (AMD64)] on win32
+# Type "help", "copyright", "credits" or "license" for more information.
+# >>> exit()
+# PS C:\Users\subrah> python3 Problem_of_day.py
+# helool inside simple34
+# hello sir
+# hello world
+# the size of the list: 104
+# pinting the memory allowactions fo the list: 2331214688640
+# ['subbu', 'hegde', 33, 667, 89, 90, 'Hello hii']
+# the size of the List after appending: 136
+# print the  or the location : 2331214688640
+# the updated list: ['subbu', 'hegde', 33, 667, 89, 90, 'Hello hii', 'sdsdsdsdsdsdsd']
+# the size: 136
+# the  find memeory locations if list: 2331214688640
+# ['subbu', 'hegde', 33, 667, 89, 90, 'Hello hii', 'sdsdsdsdsdsdsd', [2323232323, 23232323232322323]]
+# size: 136
+# id: 2331214688640
+# the empty lits checking:
+# <class 'list'>
+# 2331214688640
+# 2331215061248
+# _________________________________tuple:________
+# the size of the tuple: 96
+# the location 2331214729280
+# PS C:\Users\subrah> python3 Problem_of_day.py
+# helool inside simple34
+# hello sir
+# hello world
+# the size of the list: 104
+# pinting the memory allowactions fo the list: 2151787213184
+# ['subbu', 'hegde', 33, 667, 89, 90, 'Hello hii']
+# the size of the List after appending: 136
+# print the  or the location : 2151787213184
+# the updated list: ['subbu', 'hegde', 33, 667, 89, 90, 'Hello hii', 'sdsdsdsdsdsdsd']
+# the size: 136
+# the  find memeory locations if list: 2151787213184
+# ['subbu', 'hegde', 33, 667, 89, 90, 'Hello hii', 'sdsdsdsdsdsdsd', [2323232323, 23232323232322323]]
+# size: 136
+# id: 2151787213184
+# the empty lits checking:
+# <class 'list'>
+# 2151787213184
+# 2151787585856
+# _________________________________tuple:________
+# the size of the tuple: 96
+# the location 2151787253824
+# 2151784775744
+# 2151784775744
+# PS C:\Users\subrah> python3 Problem_of_day.py
+# helool inside simple34
+# hello sir
+# hello world
+# the size of the list: 104
+# pinting the memory allowactions fo the list: 1982493700480
+# ['subbu', 'hegde', 33, 667, 89, 90, 'Hello hii']
+# the size of the List after appending: 136
+# print the  or the location : 1982493700480
+# the updated list: ['subbu', 'hegde', 33, 667, 89, 90, 'Hello hii', 'sdsdsdsdsdsdsd']
+# the  find memeory locations if list: 1982493700480
+# the  find memeory locations if list: 1982493700480            ['subbu', 'hegde', 33, 667, 89, 90, 'Hello hii', 'sdsdsdsdsdsdsd', [2323232323, 23232323232322323]]
+# size: 136
+# id: 1982493700480
+# the empty lits checking:
+# <class 'list'>
+# 1982493700480
+# 1982495265408
+# _________________________________tuple:________
+# the size of the tuple: 96
+# the location 1982493741120
+# 1982489165888
+# 1982489165888
+# 1982494064560
+# 1982495264880
+# PS C:\Users\subrah>
+#  *  History restored 
+
+# PS C:\Users\subrah>
+
+
+
