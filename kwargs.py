4c263e3b42c4efd27e842a4373d57765f0dbdf03
pythonfor2023 <128965292+pythonfor2023@users.noreply.github.com>
2023-04-25 14:31:57 +0530

    uncommented and extra functions

--- kwargs.py
+++ kwargs.py
@@ -1,12 +1,11 @@
 
-# def myFun(ks):
-# 	for key in ks:
-# 		print(key)
-# 		# print(ks)
-# 		print(id(key))
-# 		print(type(key))
-
-# myFun("ehdee")
+def myFun(*ks): # non-key word argument
+	for key in ks:
+		print(key)
+		# print(ks)
+		print(id(key))
+		print(type(key))
+myFun("ehdee")
 
 def fun(**kwargs):
     print("displaying key world argumment")
@@ -15,6 +14,29 @@ def fun(**kwargs):
         print("key, value",key,value)
 
 fun(Key="subbu",simple="simple",parameter="meter")
+
+    # Positional arguments 
+
+def simple_function(name,age):
+    print("printing the positional argument:")
+    print("the argument")
+    print(name,age)
+
+simple_function("subbu","Hegde")
+
+# keyword arguments
+def fun(name,age):
+    print(name,age)
+    # print(key,value)
+    print(type(name))
+
+fun(name="ravi",age="34")
+
+def carValue(car_price,milage):
+    print(car_price,milage)
+
+carValue(milage=232323)
+carValue(car_price=232,milage=90909)
 
 
 
